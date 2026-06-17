4067ec27539aeddff1603432bab7525602350981
NidhafathimaKt <nidhafathima813.com>
2026-06-17 21:03:11 +0530

    Update code

--- server/server.js
+++ server/server.js
@@ -57,7 +57,16 @@ const superAdminRoute = require("./src/routes/superAdminRoute");
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://edu-prime-five.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
